d40d501692998d37ec9debe84a73b4d1b6f511d9
sgordon007 <sgordon@lbl.gov>
2018-05-20 14:13:11 -0700

    trouble shooting using function use from pip installed package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['click']
 
 setup(
     name='faketool',
-    version='0.1.3',
+    version='0.1.4',
     url='https://github.com/sgordon007/fake-tool',
     license='BSD',
     author='Sean Gordon',
